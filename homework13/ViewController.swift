248a07c89681ed89b567a68a9d8492a50a0b3353
ivan_mls <ivn.mls96@gmail.com>
2023-04-02 00:58:41 +0300

    Initial Commit

---
+++ homework13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework13
+//
+//  Created by User on 2.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
